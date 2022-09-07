12cf115c995158aec5cd07a533b28cde1692f5f5
Lucas Espindola <lucas.espindola@ext.runpanther.io>
2022-09-07 15:56:21 -0300

    stuff

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package auda
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Box struct {
 	width  int
@@ -10,6 +13,19 @@ type Box struct {
 
 type Container struct {
 	Box
+	unsortedItems []Item
+}
+
+func (c *Container) pickItems(pool *Pool) {
+	v := c.Volume()
+	for {
+		found, item := pool.SafeTakeItem(v)
+		if !found {
+			break
+		}
+		v = v - item.Volume()
+		c.unsortedItems = append(c.unsortedItems, item)
+	}
 }
 
 type Item struct {
@@ -20,15 +36,54 @@ func (b Box) Volume() int {
 	return b.height * b.width * b.length
 }
 
+type Pool struct {
+	mu    sync.Mutex
+	items []Item // this is a sorted list of item by voulme, desc
+}
+
+func NewPool(items []Item) *Pool {
+	// sort items (merge sort)
+	return &Pool{items: items}
+}
+
+func (p *Pool) SafeTakeItem(volume int) (bool, Item) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if len(p.items) == 0 {
+		return false, Item{}
+	}
+
+	selected := p.items[0]
+	if selected.Volume() <= volume {
+		p.items = p.items[1:]
+		return true, selected
+	}
+
+	return false, Item{}
+}
+
 func main() {
 	// todo: move to cmd input
-	container := Container{Box{height: 10, width: 10, length: 10}}
-	containers := breakSpace(container)
+	container := Container{Box{height: 10, width: 10, length: 10}, []Item{}}
+	items := []Item{{Box{width: 2, height: 5, length: 4}}, {Box{width: 2, height: 5, length: 4}}, {Box{width: 2, height: 5, length: 4}}, {Box{width: 2, height: 5, length: 4}}}
+	pool := NewPool(items)
+
+	containers := breakSpace(container) // serial
+
+	// here ends the serial section, from now own everything can be in parallel, besides the last segment
+
+	for _, c := range containers {
+		go c.pickItems(pool)
+	}
 
 	fmt.Println("Hello, 世界")
 }
 
 func breakSpace(full Container) []Container {
 	// todo: break the space
-	return []Container{Container{Box{height: 10, width: 10, length: 5}}, Container{Box{height: 10, width: 10, length: 5}}}
+	return []Container{
+		{Box{height: 10, width: 5, length: 10}, []Item{}},
+		{Box{height: 10, width: 5, length: 10}, []Item{}},
+	}
 }
